3f4784449563b86fc5a45414bf67b8b630d84e30
Sharath Kumar R <103100163+sharathkraj@users.noreply.github.com>
2023-02-07 16:27:21 +0530

    Exploring web driver - download,upload, iframe, multitabs

---
+++ Seleninum_course/src/test/java/Exploring_Webdriver/Download.java
@@ -0,0 +1,33 @@
+package Exploring_Webdriver;
+
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Download {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\fbdev\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.leafground.com/file.xhtml");
+		
+		WebElement download = driver.findElement(By.xpath("//*[@id=\'j_idt93:j_idt95\']/span[2]"));
+		download.click();
+		
+		File filelocation = new File("D:\\down");
+		File[] totalFiles = filelocation.listFiles();
+		
+		for (File file : totalFiles) {
+			if(file.getName().equals("TestLeafLogo.png")) {
+				System.out.println("download sucessfully");
+			     break;
+			}
+		}
+	}
+
+}
---
+++ Seleninum_course/src/test/java/Exploring_Webdriver/Iframe_Handling.java
@@ -0,0 +1,42 @@
+package Exploring_Webdriver;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Iframe_Handling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\fbdev\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.leafground.com/frame.xhtml");
+        
+		driver.switchTo().frame(0);
+		WebElement ClickMe = driver.findElement(By.xpath("/html/body/button"));
+		ClickMe.click();
+		
+		
+		
+		driver.switchTo().defaultContent();
+		
+		driver.switchTo().frame(1);
+		driver.switchTo().frame("frame2");
+		WebElement ClickMe1 = driver.findElement(By.xpath("//*[@id=\'Click\']"));
+		ClickMe1.click();
+		
+		driver.switchTo().defaultContent();
+		
+		List<WebElement> totalFrames= driver.findElements(By.tagName("iframe"));
+		
+		int size= totalFrames.size();
+		
+
+	}
+
+}
