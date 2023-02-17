3e39c6129665625977622f9dca93d34150debcb9
Sharath Kumar R <103100163+sharathkraj@users.noreply.github.com>
2023-02-17 18:58:51 +0530

    Java - Inheritance

---
+++ Java_learning/src/Inheritance_learning/Bike.java
@@ -0,0 +1,12 @@
+package Inheritance_learning;
+
+public class Bike {
+	
+	 public int wheel =2;
+	 
+	 public void engine() {
+		 
+		 System.out.println("125cc engine");
+	 }
+
+}
---
+++ Java_learning/src/Inheritance_learning/Shine.java
@@ -0,0 +1,12 @@
+package Inheritance_learning;
+
+public class Shine extends Bike {
+	
+
+	public static void main(String[] args) {
+		
+		Shine shine = new Shine();
+		shine.engine();
+	}
+
+}
